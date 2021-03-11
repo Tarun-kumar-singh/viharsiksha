11e0566924cf0b8e817a6a9c25e0b648fd984fa2
tarunks2512@gmail.com <tarun.singh@mutualmobile.com>
2021-03-11 17:23:57 +0530

    display right mark

--- src/component/comman/objectiveQuestion.jsx
+++ src/component/comman/objectiveQuestion.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
-import { Grid } from '@material-ui/core';
+import React, {useState} from 'react';
 import { Typography } from '@material-ui/core';
 import Card from '@material-ui/core/Card';
 import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import CheckIcon from '@material-ui/icons/Check';
+import CloseIcon from '@material-ui/icons/Close';
+import { green, red } from '@material-ui/core/colors';
+
 const usestyles = makeStyles(({
     card:{
             cursor:'pointer',
@@ -10,29 +14,52 @@ const usestyles = makeStyles(({
             padding:'1%',
             marginTop:'20px',
             '&:hover':{
-                border: 'solid 1px #117aca'
-             }
+                background:'#d7ded9',
+                opacity:'0.5'
+             },
+             boxShadow:'none'
         }
 }))
-const ObjectiveQuestion = (props) =>{
+const ObjectiveQuestion = (props) => {
 
     const classes = usestyles()
+    const [solved, setSolved] = useState(false)
+    const [isAnswerCorrect, setIsAnswerCorrect] = useState(undefined)
+    const [solvedOption, setSolvedOption] = useState(undefined)
+
+    const optionMap = {
+        1: 'A',
+        2: 'B',
+        3: 'C',
+        4: 'D'
+    }
+    
+    const checkAnswer = (el, index, rightOption) =>{
+        setSolved(true)
+        setSolvedOption(index)
+        setIsAnswerCorrect(index + 1 === rightOption ? true :  false)
+    }
+
     return(
-        <div>
-            <Grid direction="column" container spacing={1}>
+        <div style={{paddingTop:'50px'}}>
+            <Box display="flex" flexDirection="column" spacing={1}>
                      <Typography variant="h6" component="h6">
                         {props.question}
                     </Typography>
                  {
                     props.options.map((el, index) => (
-                    <Grid key={index} item xs={10}>
-                        <Card onClick={() => props.checkAnswer(index)} className={classes.card}> 
-                            {el}
+                         <Card onClick={() => checkAnswer(el, index, props.rightOption)} className={classes.card}> 
+                        
+                         <div style={{display:'flex', direction:'row', gap:'20px'}}>
+                             <div style={{display: solved && isAnswerCorrect && solvedOption === index ? 'block' : 'none'}}><CheckIcon style={{ color: green[500] }}/></div> 
+                             <div style={{display: solved && !isAnswerCorrect && solvedOption === index ? 'block' : 'none'}}><CloseIcon style={{ color: red[500] }} /></div>
+                            <div style={{fontWeight:700}}>{optionMap[index+1]}</div> 
+                            <div>{el}</div> 
+                        </div>
                         </Card>
-                    </Grid>
-                    ))
+                     ))
                 }
-            </Grid>
+            </Box>
         </div>
     )
 }
--- src/component/users/displayObjectiveQuestions.jsx
+++ src/component/users/displayObjectiveQuestions.jsx
@@ -4,21 +4,18 @@ import ObjectiveQuestion from "../comman/objectiveQuestion";
 
 const DisplayObjectiveQuestions = (props) => {
 
-    const q = [{question:'सोपानी कृषि किस राज्य में प्रचलित है ?', options:['बिहार का मैदानी क्षेत्र','उत्तराखंड','हरियाणा','पंजाब']},{question:'सोपानी कृषि किस राज्य में प्रचलित है ?', options:['बिहार का मैदानी क्षेत्र','उत्तराखंड','हरियाणा','पंजाब']},{question:'सोपानी कृषि किस राज्य में प्रचलित है ?', options:['बिहार का मैदानी क्षेत्र','उत्तराखंड','हरियाणा','पंजाब']}]
-   
-    const checkAnswer = (el) => {
-        console.log(el)
-    }
+    const q = [{question:'सोपानी कृषि किस राज्य में प्रचलित है ?', options:['बिहार का मैदानी क्षेत्र','उत्तराखंड','हरियाणा','पंजाब'], rightOption:1},{question:'सोपानी कृषि किस राज्य में प्रचलित है ?', options:['बिहार का मैदानी क्षेत्र','उत्तराखंड','हरियाणा','पंजाब'], rightOption:2},{question:'सोपानी कृषि किस राज्य में प्रचलित है ?', options:['बिहार का मैदानी क्षेत्र','उत्तराखंड','हरियाणा','पंजाब'], rightOption:3}]
+  
 
     return(
-        <div>
+        <div style={{marginLeft:'10%', paddingTop:'50px'}}>
             {
               q.map((el, index) => (
                 <ObjectiveQuestion 
                    key={index}              
                    question={el.question}
                    options={el.options}
-                   checkAnswer={checkAnswer}
+                   rightOption={el.rightOption}
                 />
               ))
             }
